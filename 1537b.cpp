cb25f2c42cf3ef0a04c9771aca4fb882fb260813
Dhruv <dhruv.singh_cs21@gla.ac.in>
2022-06-19 20:20:53 +0530

    june19

---
+++ 1537b.cpp
@@ -0,0 +1,12 @@
+// 1537b.cpp
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	int t;cin>>t;
+	while(t--){
+		long long i,j,m,n;
+		cin>>n>>m>>i>>j;
+		cout<<1<<" "<<1<<" "<<n<<" "<<m<<"\n";
+	}
+	return 0;
+}
