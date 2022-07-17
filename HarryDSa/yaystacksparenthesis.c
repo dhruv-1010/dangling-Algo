c8125d9e0a080923a12a31dc975bb0bff21c191a
Dhruv <dhruv.singh_cs21@gla.ac.in>
2022-07-17 08:43:38 +0530

    QQQQ

---
+++ HarryDSa/yaystacksparenthesis.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct stack{
+    int size;
+    int top;
+    int* arr;
+}stack;
+
+int main(){
+    stack* mainstack =(stack*) malloc(sizeof(stack)); 
+    
+    return 0;
+
+}
