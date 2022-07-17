8bebf1b784e65a0c4e34fa90c23202eef9697403
Dhruv <dhruv.singh_cs21@gla.ac.in>
2022-07-17 17:45:49 +0530

    Queue and more

---
+++ HarryDSa/queuetwo.c
@@ -0,0 +1,56 @@
+// appraoch one -> using top element keeping track of index of inserted element and the first element
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct q{
+    int size;
+    int top;
+    int* arr;
+}queue;
+void enque(queue* ptr,int n){
+    ptr->top++;
+    ptr->arr[ptr->top] = n;
+    printf("enque done \n");
+}
+void print(queue* ptr){
+    int i;
+    for(i = 0;i<=(ptr->top);i++){
+        int a = ptr->arr[i];
+        printf("%d ",a);
+    }printf("\n");
+}
+void deque(queue* ptr){
+    ptr->arr[ptr->top] = 0;
+    ptr->top--;
+    printf("deque done \n");
+}
+int first_val(queue* ptr){
+    return ptr->arr[0];
+}
+
+int last_val(queue* ptr){
+    return ptr->arr[ptr->top];
+}
+int peek(queue* ptr,int pos){
+    return ptr->arr[pos-1];
+}
+int main(){
+    queue* mq =(queue*) malloc(sizeof(queue));
+    mq->size=5;
+    mq->arr = (int*) malloc(5*sizeof(int));
+    mq->top = -1;
+    enque(mq,5);
+    enque(mq,4);
+    enque(mq,3);
+    enque(mq,2);
+    enque(mq,1);
+    print(mq);
+    // deque(mq);
+    // print(mq);
+    // deque(mq);
+    // print(mq);
+    int fv = first_val(mq);
+    int lv = last_val(mq);
+    int pee = peek(mq,4);
+    printf("%d %d %d",fv,lv,pee);
+    return 0;
+}
--- HarryDSa/yaystacksparenthesis.c
+++ HarryDSa/yaystacksparenthesis.c
@@ -5,10 +5,35 @@ typedef struct stack{
     int top;
     int* arr;
 }stack;
+char stacklast(stack* m){
+    return m->arr[m->top];
+}
+int solve(stack* stackk,char array[],int size){
+    int i;
+    for(i=0;i<size;i++){
+        if(array[i]=="("){
+            stackk->top++;
+            stackk->arr[stackk->top] = "(";
+        }
+        if(array[i]=="("){
+            char ch = stacklast(stackk);
+            if(ch!=")")return 0;
+        }
+    }
+    return 1;
+}
 
 int main(){
     stack* mainstack =(stack*) malloc(sizeof(stack)); 
-    
+    mainstack->top=-1;
+    int size=9;
+    // printf("stack size \n"); i can take inputs but that is not imp here
+    // scanf("%d",&size);       concept of parenthesis matching is
+    // printf("enter your ")
+    mainstack->arr = (char *) malloc(size*sizeof(char));
+    char chr[INT_MAX] = "((5+5+6))";
+    printf("code begin \n");
+    solve(mainstack,chr,size);
     return 0;
 
 }
