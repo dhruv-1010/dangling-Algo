9f5f4c02f53a41e75d3b4fb96259bc63ccaedf56
Dhruv <dhruv.singh_cs21@gla.ac.in>
2022-07-19 18:03:49 +0530

    Added files via Upload

--- luv/d3.cpp
+++ luv/d3.cpp
@@ -1,16 +1,22 @@
+// buggy code down dont commit
+#include <bits/stdc++.h>
+
+using namespace std;
+int main(){
+    vector<int> a(5);
+    for(int i:a){
+        cin>>a[i];
+    }
+    // vector functions
+    for(int i:a){
+        cout<<a[i]<<" ";
+        // auto doesnt work am i wrong?
+    }
+    return 0;
+}
 // STL Begins.
 // main topics in STL.
 // containers -- structures prebuilt like vectors,map.
 // iterators -- pointer type object -- begin end rbegin rend some more auto keyword,random and many more
 // some famous algorithms -- swap,sum,max,min,max_element,min_element and their return types
 // // functors [not in use ] basically are just classes that can oprate as functions
-#include <bits/stdc++.h>
-using namespace std;
-int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        cout<<"kawaaaiii mono ";
-    }
-    return 0;
-}
