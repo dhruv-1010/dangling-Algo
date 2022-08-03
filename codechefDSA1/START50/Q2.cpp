ec714b0c4a7031ef66f35b89716fe49cf6abb440
Dhruv Singh <96680040+dhruv-1010@users.noreply.github.com>
2022-08-03 20:33:52 +0530

    Create Q2.cpp

---
+++ codechefDSA1/START50/Q2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        float n,x,y,a,b;
+        cin>>n>>x>>y>>a>>b;
+        if((x/a)>(y/b)) cout<<"DIESEL"<<'\n';
+        else if((x/a)<(y/b)) cout<<"PETROL"<<'\n';
+        
+        else cout<<"ANY"<<'\n';
+        
+    }
+	// your code goes here
+	return 0;
+}
