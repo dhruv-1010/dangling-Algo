08c34abda84b65fecbec04addfcbd2a20c4f1aee
Dhruv Singh <96680040+dhruv-1010@users.noreply.github.com>
2022-08-13 01:36:57 +0530

    Update Q2.cpp

--- codechefDSA1/LONG221/Q2.cpp
+++ codechefDSA1/LONG221/Q2.cpp
@@ -3,9 +3,20 @@ using namespace std;
 #define endl "\n"
 #define mod 1000000007
 #define op  ios_base::sync_with_stdio(false);cin.tie(NULL);
+typedef long long  ll;aa#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define mod 1000000007
+#define op  ios_base::sync_with_stdio(false);cin.tie(NULL);
 typedef long long  ll;
 typedef long l;
 void solve(){
+    ll a;
+    cin>>a;
+    if(a<=100) cout<<a<<endl;
+    else if(a>100 && a<=1000) cout<<a-25<<endl;
+    else if(a>1000 && a<=5000) cout<<a-100<<endl;
+    else cout<<a-500<<endl;
 }
 int main() {
     op;
@@ -17,3 +28,4 @@ int main() {
 	}
 	return 0;
 }
+
